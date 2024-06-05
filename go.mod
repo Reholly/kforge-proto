a879fe6ab574e760344915842730e05892b6c6d9
Reholly <eu.ustiantsev@yandex.ru>
2024-06-05 09:57:18 +0500

    change package structure

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module kforge-proto
 go 1.22.0
 
 require (
-	github.com/golang/protobuf v1.5.4
 	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.34.1
 )
--- src/gen/auth/auth.pb.go
+++ src/gen/auth/auth.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.12.4
 // source: proto/auth.proto
 
-package api
+package auth
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
